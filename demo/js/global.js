55034cdc407c1bdddc7b6ef52e37af3a1fec87fc
Wouter Vandenneucker <wouter@vandenneucker.be>
2016-07-28 10:07:08 +0200

    actual fix for both ipad and android

--- demo/js/global.js
+++ demo/js/global.js
@@ -9,12 +9,23 @@
     });
 
     var bindCollapsibles = function() {
+      // For use within normal web clients
+      var isiPad = navigator.userAgent.match(/iPad/i) != null;
 
-    	$(document).on("click touchstart",".n-collapsible-toggler",function(){
+      if( !isiPad ){
 
-    		$(this).closest(".n-collapsible-wrapper").children(".n-collapsible-content").toggle();
+      	$(document).on("click",".n-collapsible-toggler",function(){
 
-    	});
+      		$(this).closest(".n-collapsible-wrapper").children(".n-collapsible-content").toggle();
+
+      	});
+      } else {
+        $(document).on("touchstart",".n-collapsible-toggler",function(){
+
+      		$(this).closest(".n-collapsible-wrapper").children(".n-collapsible-content").toggle();
+
+      	});
+      }
 
     }
 
